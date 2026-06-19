dba6a7cb2d8d0f66554bdf0629a0de66e96716d2
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-06-20 01:12:15 +0300

    Implement LINQ-like MyWhere using yield return

---
+++ Day4_myLINQ/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Day4_myLINQ
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> values, Func<T, bool> predicate)
+        {
+
+            foreach (var item in values) {
+                
+                if (predicate(item))
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ Day4_myLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day4_myLINQ
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {1, 10, 15, 12, 3, 2 };
+            var result = list.MyWhere(x => x >= 10 );
+
+            Console.WriteLine("result: " + result.First() );
+
+            foreach (var item in result) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
