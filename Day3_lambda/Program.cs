9be5905d0b3b06453cb3c3babe10e459ca9501e7
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-06-19 15:22:29 +0300

    Add usual IsGreaterThan10() method

---
+++ Day3_lambda/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day3_lambda
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<int, bool> check = IsGreaterThan10;
+            bool result = check(15);
+
+            Console.WriteLine(result);
+        }
+
+        static bool IsGreaterThan10(int number)
+        {
+            return number > 10;
+        }
+    }
+}
