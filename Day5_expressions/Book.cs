febd38405057f71b01ad5d9d7f158423a8b96554
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-06-23 23:51:22 +0300

    feat: explore expression tree structure

---
+++ Day5_expressions/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Day5_expressions
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+    }
+}
---
+++ Day5_expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace Day5_expressions
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Expression<Func<Book, bool>> expr = b => b.Pages > 100;
+
+            Console.WriteLine("Expression: ");
+            Console.WriteLine(expr);
+            Console.WriteLine(expr.Body);
+            Console.WriteLine(expr.Body.NodeType);
+            Console.WriteLine(expr.Body.GetType());
+
+            Console.WriteLine("Expression body: ");
+            var body = (BinaryExpression)expr.Body;
+            var left = (MemberExpression)body.Left;
+            var right = (ConstantExpression)body.Right;
+
+            Console.WriteLine("Expression body left: ");
+            Console.WriteLine(body.Left);
+            Console.WriteLine("Expression body left type: ");
+            Console.WriteLine(body.Left.GetType().Name);
+
+            Console.WriteLine("Expression body right: ");
+            Console.WriteLine(body.Right);
+            Console.WriteLine("Expression body right type: ");
+            Console.WriteLine(body.Right.GetType().Name);
+
+            Console.WriteLine(left.Member.GetType().Name); //RuntimePropertyInfo
+        }
+    }
+}
