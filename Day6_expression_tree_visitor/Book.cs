6edb506e9ed8edddaed28cbd2b1b6b6393582896
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-06-24 13:50:44 +0300

    Make and run delegate from expression via Compile

---
+++ Day6_expression_tree_visitor/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Day6_expression_tree_visitor
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+    }
+}
---
+++ Day6_expression_tree_visitor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Day6_expression_tree_visitor
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Expression<Func<Book, bool>> expr = b => b.Pages > 100;
+            var result = expr.Compile()(new Book() { Title = "Harry Potter", Pages = 400 });
+            Console.Write(result);
+        }
+    }
+}
